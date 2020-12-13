468537601896b5a493b2bba49cf09a1849734daf
Naiem Gafar <naiemgafar@Naiems-MBP.home>
2020-12-12 21:28:37 -0500

    finalmente

--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -8,13 +8,18 @@ from apps.gardens.models import Garden, Tier
 from apps.portfolio.models import Holding
 from apps.userauth.models import UserProfile
 
+# Function: read_portfolio
+# Description: Allows patrons to view all of their investments
+
 @login_required
 def read_portfolio(request):
 	context_dict = {}
 	
+	# Get the current user and all of their holdings
 	current_user = UserProfile.objects.get(user=request.user)
 
 	holdings = Holding.objects.filter(owner=current_user)
 	context_dict['holdings'] = holdings
 
+	# Displays their holdings on the HTML page
 	return render(request, 'portfolio/read-portfolio.html', context_dict)
