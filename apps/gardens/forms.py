bf50b9c081da4d29e05977a7b7486b1d4cbb4083
Naiem Gafar <naiemgafar@Naiems-MBP.home>
2020-12-12 21:26:34 -0500

    finalmente

--- apps/gardens/forms.py
+++ apps/gardens/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from apps.gardens.models import Garden, Tier, Update, Comment, Album
 import datetime
 
+# Allows for the datepicker to be displayed on the HTML form 
 class DateInput(forms.DateInput):
     input_type = 'date'
 
+# Form for gardeners to create a new garden
 class GardenForm(forms.ModelForm):
     class Meta:
         model = Garden
@@ -16,17 +18,20 @@ class GardenForm(forms.ModelForm):
         widgets = {'sponsor_deadline': DateInput()}
         exclude = ['amount_raised', 'total_backers', 'date_created', 'active']
 
+# Form for gardeners to create a new tier that belongs to their garden
 class TierForm(forms.ModelForm):
     class Meta:
         model = Tier
         widgets = {'estimated_harvest': DateInput()}
         exclude = ['total_shares_remaining', 'num_backers']
 
+# Form for gardeners to send updates to their patrons
 class UpdateForm(forms.ModelForm):
     class Meta:
         model = Update
         exclude = ['date_published']
 
+# Form for anyone (patrons/gardeners) to comment back on updates
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
@@ -39,6 +44,7 @@ class CommentForm(forms.ModelForm):
 
         exclude = ['date_published']
 
+# Form for gardeners to create upload images to their garden profile
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = Album
