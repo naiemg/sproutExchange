b7a01d8370d37d6a04afa1e500aea3900190ad91
Naiem Gafar <naiemgafar@Naiems-MBP.home>
2020-12-12 20:56:56 -0500

    finalmente

--- apps/exchange/forms.py
+++ apps/exchange/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from apps.exchange.models import Order, Listing
 
+# Form for patron placing order on shares of an existing garden
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
@@ -10,6 +11,7 @@ class OrderForm(forms.ModelForm):
 
         exclude = []
 
+# Form for patron creating a listing to sell their existing shares
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
--- apps/exchange/urls.py
+++ apps/exchange/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from apps.exchange import views
 
 urlpatterns = [
+	# Orders
 	path('create-order/<int:tier_id>/', views.create_order),
 	path('order/<str:order_id>/', views.read_order),
 	
+	# Listings
 	path('create-listing/<int:tier_id>/', views.create_listing),
 	path('delete-listing/<int:listing_id>/', views.delete_listing),	path('delete-listing/<int:listing_id>/', views.delete_listing),
 
+	# Sales
 	path('purchase-listing/<int:listing_id>/', views.purchase_listing),
 	path('all-listings/', views.all_listings),
 ]
